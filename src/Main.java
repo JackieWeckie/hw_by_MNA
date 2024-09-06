cd6481bf17eefd8a1cf390c7bd8352ecd4cdd70f
JackieWeckie <nekit4568i@gmail.com>
2024-09-06 19:28:09 +0300

    An update from 06/09/2024

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
 public class Main {
     public static void main(String[] args) {
+        /*System.out.println("Hello world!");
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        System.out.println("Hello world!");*/
+        //31st august
+
+        
+        int i = 32;
+        short sh = 49;
+        byte b = 9;
+        char a = 'a';
+        long l = 241;
+        double doub = 7.135;
+        float fl = 8.351F;
+
+        int fst = 12;
+        int scnd = 4;
+
+        int sum = fst + scnd; //сложение
+        int diff = fst - scnd; //вычитание
+        int product = fst * scnd; //умножение
+        int quotient = fst / scnd; //деление
+        int remind = fst % scnd; //остаток от деления
+
+        System.out.println("Сложение: " + sum);
+        System.out.println("Вычитание: " + diff);
+        System.out.println("Умножение: " + product);
+        System.out.println("Деление: " + quotient);
+        System.out.println("Остаток от деления: " + remind);
         }
     }

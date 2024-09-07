ab762759c92b83192cfddc69aac62bd81eb2fcf0
JackieWeckie <nekit4568i@gmail.com>
2024-09-07 23:30:24 +0300

    changes from 07/09/2024

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        /*System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");*/
-        //31st august
-
-        
         int i = 32;
         short sh = 49;
         byte b = 9;
@@ -28,5 +25,29 @@ public class Main {
         System.out.println("Умножение: " + product);
         System.out.println("Деление: " + quotient);
         System.out.println("Остаток от деления: " + remind);
-        }
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите число: ");
+        int numb = sc.nextInt();
+
+        Random rand = new Random();
+        int randnumb1 = rand.nextInt(2418);
+        int randnumb2 = rand.nextInt(2418);
+        int randnumb3 = rand.nextInt(2418);
+        int randnumb4 = rand.nextInt(2418);
+
+        //преобразование в char
+
+        char ch1 = (char) randnumb1;
+        char ch2 = (char) randnumb2;
+        char ch3 = (char) randnumb3;
+        char ch4 = (char) randnumb4;
+
+        System.out.print("ваш код: " + ch1 + ch2 + ch3 + ch4);
+
+        long l1 = 12_342_132_761L;
+        int intNumber = (int) l1;
+        System.out.println(intNumber);
+     }
     }

808420e7cb40d531ef34cb7b68734e9943cfd1c5
JackieWeckie <nekit4568i@gmail.com>
2024-10-17 21:51:47 +0300

    changes from 17/10/2024

--- src/oct13_2024_hw/Main.java
+++ src/oct13_2024_hw/Main.java
@@ -2,22 +2,20 @@ package oct13_2024_hw;
 
 public class Main {
     public static void main(String[] args) {
-        Book book1 = new Book();
-        Book book2 = new Book();
-        Author author1 = new Author();
-        Author author2 = new Author();
+      Author author1 = new Author("John", "Tolkien");
+      Book book1 = new Book("Leaf by Niggle", author1, 1945);
+      Book book2 = new Book("Pride and Prejudice", new Author("Jane", "Austen"), 1813);
 
-        author1.setName("John");
-        author1.setSurname("Tolkien");
-        author2.setName("George");
-        author2.setSurname("Martin");
-        book1.setTitle("The Lord of the Rings");
-        book1.setYearPublished(1954);
-        book1.setAuthor(author1.getAuthorInfo());
-        book2.setTitle("Game of Thrones");
-        book2.setYearPublished(1996);
-        book2.setAuthor(author2.getAuthorInfo());
-        System.out.println(book1.getBookInfo());
-        System.out.println(book2.getBookInfo());
+      book1.setTitle("A leaf");
+      book1.setYearPublished(1946);
+      book2.setAuthor(new Author("Nikita", "Morozov"));
+
+        System.out.println("Title: " + book1.getTitle());
+        System.out.println(book1.getAuthor());
+        System.out.println("Publication year: " + book1.getYearPublished().toString());
+
+        System.out.println("\nTitle: " + book2.getTitle());
+        System.out.println(book2.getAuthor());
+        System.out.println("Publication year: " + book2.getYearPublished().toString());
     }
 }

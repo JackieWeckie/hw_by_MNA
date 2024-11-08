4c8e9f14154aa5f252238fa1301acf35d38fc77d
JackieWeckie <nekit4568i@gmail.com>
2024-11-08 15:29:41 +0300

    changes from 08/11/2024

--- src/oct29_2024_hw/Main.java
+++ src/oct29_2024_hw/Main.java
@@ -2,37 +2,24 @@ package oct29_2024_hw;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Main {
     public static void main(String[] args) {
-        Player player1 = Player.addNewPlayer();
-        Player player2 = Player.addNewPlayer();
-        Player player3 = Player.addNewPlayer();
-        Player.info();
+        Game game = new Game();
 
-        Player player4 = Player.addNewPlayer();
-        Player player5 = Player.addNewPlayer();
-        Player player6 = Player.addNewPlayer();
-        Player.info();
+        Collection<Player> players = new ArrayList<>(2);
+        System.out.println(players.isEmpty());
+        System.out.println(players);
 
-        makeTired(player2, player3);
-        Player.info();
-        makeTired(player6, player1);
-        Player.info();
-
-        Player player7 = Player.addNewPlayer();
-        Player player8 = Player.addNewPlayer();
-        Player player9 = Player.addNewPlayer();
-        Player.info();
-    }
-
-    static void makeTired(Player... players) {
-        for (Player player : players) {
-            if (player == null) {
-                continue;
-            }
-            if (!player.isTired()) {
-                player.run();
-            }
-        }
+        game.addANewPlayer();
+        game.addANewPlayer();
+        game.addANewPlayer();
+        game.addANewPlayer();
+        game.addANewPlayer();
+        game.addANewPlayer();
+        
+        game.addANewPlayer();
     }
 }
